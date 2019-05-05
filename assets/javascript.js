e62921a4b8aed4c9a402074a730100cd231c910d
Mintewab <demissiewminte123@gmail.com>
2019-05-04 19:21:02 -0700

    created for loop

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,12 @@
 var topics = ["The Matrix", "The Notebook", "Mr. Nobody", "The Lion King", "Heathers"];
 function renderButtons() {
     $("#buttons-view").empty();
+
+    for (var i = 0; i < topics.length; i++) {
+        var a = $("<button>");
+        a.addClass("tech");
+        a.attr("data-name", topics[i]);
+        a.text(topics[i]);
+        $("#buttons-view").append(a);
+    }
+});
