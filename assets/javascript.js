ca8859bd17c05684f73579afb9e72416e18a2880
Mintewab <demissiewminte123@gmail.com>
2019-05-04 19:40:24 -0700

    console.log query

--- assets/javascript.js
+++ assets/javascript.js
@@ -21,7 +21,10 @@ function renderButtons() {
             url: queryURL,
             method: "GET"
         })
-   
+        .then(function (response) {
+            console.log(queryURL);
+
+            console.log(response);
 
 
 
